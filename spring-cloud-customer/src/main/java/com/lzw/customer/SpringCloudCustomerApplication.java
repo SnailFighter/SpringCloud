9f61be11d39da485bc9eac9b6d4780aa3d01a898
SnailFighter <cht_feng@163.com>
2018-05-15 22:22:19 +0800

    添加熔断器Hystrix（一）：

--- spring-cloud-customer/src/main/java/com/lzw/customer/SpringCloudCustomerApplication.java
+++ spring-cloud-customer/src/main/java/com/lzw/customer/SpringCloudCustomerApplication.java
@@ -2,6 +2,7 @@ package com.lzw.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "com.lzw.**")
 @EnableAutoConfiguration
 @EnableFeignClients(basePackages = "com.lzw.**")
+@EnableCircuitBreaker
 public class SpringCloudCustomerApplication {
 
 	public static void main(String[] args) {
--- spring-cloud-customer/src/main/java/com/lzw/customer/controller/CustomerController.java
+++ spring-cloud-customer/src/main/java/com/lzw/customer/controller/CustomerController.java
@@ -2,6 +2,8 @@ package com.lzw.customer.controller;
 
 import com.lzw.customer.apis.Hello;
 import com.lzw.apis.Study;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +25,31 @@ public class CustomerController {
     }
 
     @PostMapping("/study/")
+    @HystrixCommand(commandProperties = {
+            //可以这么设置
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "20"),
+    },
+            //也可以这样设置
+            threadPoolProperties = {
+                   /* @HystrixProperty(name = "coreSize", value = "30"),
+                    @HystrixProperty(name = "maxQueueSize", value = "101"),
+                    @HystrixProperty(name = "keepAliveTimeMinutes", value = "2"),
+                    @HystrixProperty(name = "queueSizeRejectionThreshold", value = "15"),*/
+                    @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "20"),
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000")
+            },fallbackMethod = "defaultStudy")
     public String study(@RequestBody  String name){
         return study.study(name);
     }
+    //如果参数与sdudy参数不一致，报错
+    public String defaultStudy1(){
+        return "study...";
+    }
+
+    public String defaultStudy(@RequestBody  String name){
+        return "study...";
+    }
+
+
 }
